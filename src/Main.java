7584852f9dc55e80fe91e3debc5f208eff4ecffe
Z3alotik <11bart11@email.cz>
2023-06-05 11:42:31 +0200

    Initial commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import crawler.Crawler;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Crawler crawler = new Crawler();
+        crawler.crawl("https://agrp.dev");
     }
 }
